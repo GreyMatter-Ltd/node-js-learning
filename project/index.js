59d6a7c0fa7379056bad9b9128838f4a2305ee27
unknown <arnob01611@gmail.com>
2022-05-21 23:24:03 +0600

    #dev01

--- project/index.js
+++ project/index.js
@@ -1,6 +1,14 @@
-let x = 100;
+//#dev01 -> How to create a server with express and basic events
 
-if (x === 100)
+const express = require('express'); // importing express
+
+const app = express(); // making a variable for using express
+
+const server = app.listen(3000, () => //using 3000 as port
 {
-    console.log('test')
-}
+    console.log('server is running..'); // getting a confirmation if server is running
+});
+
+app.get('/', (req, res) =>{
+    res.send('Home Pag')
+})
